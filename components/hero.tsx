7f4ac7df33c809840d2e00f44657925f96cf1d56
Bhavishya <121146811+bhavishya2324@users.noreply.github.com>
2024-03-22 22:09:09 +0530

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -19,20 +19,20 @@ export default function Hero() {
           </svg>
         </div>
 
-        {/* Hero content */}
+         {/* Hero content */}
         <div className="relative pt-32 pb-10 md:pt-40 md:pb-16">
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h1 className="h1 mb-4" data-aos="fade-up">Step into the Future with Noxiz </h1>
+            <h1 className="h2 mb-4" data-aos="fade-up">Step into the Future with Noxiz </h1>
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Your Gateway to Next-Gen Solutions! 🚀🌌</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="/About">ABOUT US</a>
+                <a className="btn text-xl text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://calendly.com/noxiz/30-minute-meeting">Schedule Consultation</a>
               </div>
-              <div data-aos="fade-up" data-aos-delay="600">
+              {/* <div data-aos="fade-up" data-aos-delay="600">
                 <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="/Contact">CONTACT US</a>
-              </div>
+              </div> */}
             </div>
           </div>
 
@@ -51,3 +51,4 @@ export default function Hero() {
     </section>
   )
 }
+
